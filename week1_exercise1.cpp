e8e1690824b89d4101b27afa5a1ece113ede8177
Kevin Yuan <k2yuan@ucsd.edu>
2025-10-19 14:46:30 -0700

    Completed exercise 1

--- week1_exercise1.cpp
+++ week1_exercise1.cpp
@@ -4,11 +4,19 @@
 //IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
 
+DigitalOut output(LED1);
+DigitalIn input(BUTTON1);
+
 int main() {
     while (1) {
-        printf("Week 1 Exercise 1");
+        int x = input.read();
 
-        // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
-        wait_ms(500); 
+        if (x == 1) {
+            output = 1; //On
+        } else {
+            output = 0;  //Off 
+        }
+
+        wait_ms(500); //Delay
     }
 }
